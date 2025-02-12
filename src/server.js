2a40d73fb05ad55303697cde5306cabb945bbb7a
Efrino Wahyu Eko Pambudi <efrinowep@gmail.com>
2025-02-12 22:15:24 +0700

    [done]menyesuaikan testing postman

--- src/server.js
+++ src/server.js
@@ -1,22 +1,17 @@
-'use strict';
-
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
-
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: 9000,
+        host: 'localhost',
+        routes: { cors: { origin: ['*'] } },
     });
 
+    server.route(routes);
+
     await server.start();
-    console.log('Server running on %s', server.info.uri);
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
 
 init();
